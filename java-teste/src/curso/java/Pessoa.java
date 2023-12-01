b3681ac5b65ac3595ea78715b2a1bf60149873a6
Eric Vieira Pereira <ericvieira.p@outlook.com>
2023-11-30 23:13:53 -0300

    Commit: Adicionando o atributo endereco

--- java-teste/src/curso/java/Pessoa.java
+++ java-teste/src/curso/java/Pessoa.java
@@ -1,9 +1,19 @@
 package curso.java;
 
 public class Pessoa {
-	
+
 	private String nome;
-	
+
+	private String endereco;
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
 	public String getNome() {
 		return nome;
 	}
